28a47451b068455e57eb17f398d46810de86e486
Florian Knop <florian_knop@hotmail.com>
2017-04-15 16:39:05 +0200

    spec: import all core-js

--- hook.spec.ts
+++ hook.spec.ts
@@ -5,7 +5,7 @@ import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
-import 'core-js/es7/reflect';
+import 'core-js';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
