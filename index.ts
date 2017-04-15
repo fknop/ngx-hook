ac11c7ecd2f5b85b5d47af38865e3f0c1c99a3e8
Florian Knop <florian_knop@hotmail.com>
2017-04-15 17:24:17 +0200

    refactor: clean

--- index.ts
+++ index.ts
@@ -1,34 +1,27 @@
 import { EventEmitter } from '@angular/core';
 
-
 export type LifecycleHook = 
   'ngOnInit' | 'ngOnDestroy' | 'ngOnChanges' | 'ngDoCheck' | 
   'ngAfterContentInit' | 'ngAfterContentChecked' | 
   'ngAfterViewInit' | 'ngAfterViewChecked';
 
 export function Hook (hook: LifecycleHook = 'ngOnDestroy') {
+
   return function (target: any, key: any) {
-
-    const oldNgOnInit = target['ngOnInit'];
-    target['ngOnInit'] = function () {
-      if (!this[key]) {
-        this[key] = new EventEmitter<any>();
-      }
-
-      if (oldNgOnInit) {
-        oldNgOnInit.bind(this)();
-      }
-    }
-
+    
+    const ngOnInit = target['ngOnInit'];
     const old = target[hook];
+
+    target['ngOnInit'] = function () {
+
+      this[key] = this[key] || new EventEmitter<any>();
+      if (ngOnInit) { ngOnInit.call(this); }
+    }
 
     target[hook] = function () {
 
-      if (old) {
-        old.bind(this)();
-      }
-
-      if (this[key] && this[key].next) {
+      if (old) { old.call(this); }
+      if (this[key] && this[key].next && this[key].subscribe) {
         this[key].next();
       }
     }
